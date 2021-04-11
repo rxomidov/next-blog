b865d9bd54603f9abad3fcc1564de43bde5b5ac6
@rixsitilloxomidov <rixsitilloxomidov@gmail.com>
2021-04-11 12:13:36 +0500

    second commit

---
+++ pages/posts/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            <h1>This is Posts page!</h1>
+
+
+            <style jsx>{`
+h1{
+  color: lightcoral;
+}
+    `}</style>
+        </div>
+    );
+};
+
+export default Index;
